a3a2347bb069bef78908c33544f3c3af3dce50d0
TahsinAhammed16 <tahsin.ahammed16@gmail.com>
2024-11-16 21:14:10 +0600

    implemented admin login with menu navigation and log out feature

--- main.cpp
+++ main.cpp
@@ -5,9 +5,24 @@
 
 using namespace std;
 
+class Admin
+{
+public:
+    string adminUsername = "a";
+    string adminPassword = "a";
+
+    bool login(string username, string password)
+    {
+        return (username == adminUsername && password == adminPassword);
+    }
+};
+
 int main()
 {
-    int choice;
+    Admin admin;
+    int choice, adminChoice;
+    string username, password;
+
     while (true)
     {
         cout << "\n===============================" << endl;
@@ -24,6 +39,62 @@ int main()
         switch (choice)
         {
         case 1:
+            cout << "\n===============================" << endl;
+            cout << "        Admin Login   " << endl;
+            cout << "===============================" << endl;
+            cout << "Enter Admin Username: ";
+            cin.ignore(); // To clear the input buffer
+            getline(cin, username);
+            cout << "Enter Admin Password: ";
+            getline(cin, password);
+            if (admin.login(username, password))
+            {
+                system("cls");
+                cout << "Admin logged in successfully!" << endl;
+                bool isAdminLoggedIn = true; // Boolean flag to control the loop
+                while (isAdminLoggedIn)
+                {
+                    cout << "\n===============================" << endl;
+                    cout << "        Admin Menu   " << endl;
+                    cout << "===============================" << endl;
+                    cout << "1. Register Student" << endl;
+                    cout << "2. Set Marks for Students" << endl;
+                    cout << "3. View All Registered Student Records" << endl;
+                    cout << "4. Display Individual Student Profile/Generate Grade Sheet" << endl;
+                    cout << "5. Update Student Data" << endl;
+                    cout << "6. Delete Student Data" << endl;
+                    cout << "7. Log Out" << endl;
+                    cout << "\nEnter your choice: ";
+                    cin >> adminChoice;
+                    system("cls");
+
+                    switch (adminChoice)
+                    {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        cout << "Admin logged out successfully!" << endl;
+                        isAdminLoggedIn = false; // Set flag to false to exit loop
+                        break;
+                    default:
+                        cout << "Invalid choice, please try again." << endl;
+                    }
+                }
+            }
+            else
+            {
+                cout << "Invalid Admin credentials, returning to main menu." << endl;
+            }
             break;
         case 2:
             break;
